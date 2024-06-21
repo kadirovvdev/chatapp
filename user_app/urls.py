fa6ed01028d902f8b48e9e1ba2c6674254c4b9b6
Jahongir <jahongirq702@gmail.com>
2024-06-21 15:42:22 +0500

    login

--- user_app/urls.py
+++ user_app/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 urlpatterns = [
     path("signup/", views.user_signup, name="signup"),
-    path("login", views.user_login, name="login"),
-    path("logout", views.user_logout, name="logout"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
 ]
