fed8c2799e2317000247fa5375e1c9ce7f123eb6
Jahongir <jahongirq702@gmail.com>
2024-06-21 15:54:46 +0500

    server

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&ria0jkpe&dh8ijb6_ync17b!li*m)=!em7bz0=93bazb+a9vr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_ROOT = 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
